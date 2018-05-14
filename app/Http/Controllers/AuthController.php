f5675307357c22592da5e1b9c60e1d9ce5b36a3b
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-05-14 16:48:50 -0300

    Changed the gettoken function to display the token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -42,7 +42,36 @@ class AuthController extends Controller
 
         // Authorization code should be in the "code" query param
         if (isset($_GET['code'])) {
-            echo 'Auth code: '.$_GET['code'];
+            // Check that state matches
+            if (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth_state'])) {
+                exit('State provided in redirect does not match expected value.');
+            }
+
+            // Clear saved state
+            unset($_SESSION['oauth_state']);
+
+            // Initialize the OAuth client
+            $oauthClient = new \League\OAuth2\Client\Provider\GenericProvider([
+                'clientId'                => env('OAUTH_APP_ID'),
+                'clientSecret'            => env('OAUTH_APP_PASSWORD'),
+                'redirectUri'             => env('OAUTH_REDIRECT_URI'),
+                'urlAuthorize'            => env('OAUTH_AUTHORITY').env('OAUTH_AUTHORIZE_ENDPOINT'),
+                'urlAccessToken'          => env('OAUTH_AUTHORITY').env('OAUTH_TOKEN_ENDPOINT'),
+                'urlResourceOwnerDetails' => '',
+                'scopes'                  => env('OAUTH_SCOPES')
+            ]);
+
+            try {
+                // Make the token request
+                $accessToken = $oauthClient->getAccessToken('authorization_code', [
+                'code' => $_GET['code']
+            ]);
+
+            echo 'Access token: '.$accessToken->getToken();
+            }
+                catch (League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+                exit('ERROR getting tokens: '.$e->getMessage());
+            }
             exit();
         }
         elseif (isset($_GET['error'])) {
